6c04135b62b18d96e07f98e7cdd349a305b43618
sdlqm456-ctrl <sdlqm456@gmail.com>
2026-01-22 23:19:03 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2/promise");
 
-// DB 연결 설정
+// DB 연결 설정 (설정을 한 곳에 모으기)
 // pool 생성
 const pool = mysql.createPool({
+  // DB 연결을 여러개 미리 만들어 줌
   host: "localhost",
   user: "dev01",
   password: "dev01",
   database: "dev",
 });
-module.exports = pool;
+module.exports = pool; // pool을 다른 파일에서도 사용가능하게 내보내기
