b598d37612e63e4dd0183186be55946dcbc26c08
sdlqm456-ctrl <sdlqm456@gmail.com>
2026-01-22 23:22:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-// app.js (1.서버를 실행)
+// app.js
 
-const express = require("express");
+// 1. express 불러오기 (서버제작)
+// require: 다른 모듈 불러오기
+const express = require("express"); // express 프레임 워크 가져오기 (서버를 만드는 도구)
 const boardRoute = require("./routes/board.routes");
 
-const app = express(); // 인스턴스
+const app = express(); // 인스턴스 (서버생성)
 app.use(express.json()); // body 영역의 데이터를 json 포맷으로 해석
-app.use("/boards", boardRoute); // 게시판 라우팅
+app.use("/boards", boardRoute); // 게시판 라우터 연결
 
-// 라우팅 정보
+// 기본 라우트
 // '/': URL정보
 app.get("/", (rep, res) => {
   res.send("/경로호출");
 });
 
+// 서버 실행
 app.listen(3000, () => {
   console.log("서버실행 http://localhost:3000");
 });
