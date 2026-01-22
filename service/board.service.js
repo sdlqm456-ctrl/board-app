c1e474924b9d46c870a4917eaa51c6a94f1e5642
sdlqm456-ctrl <sdlqm456@gmail.com>
2026-01-22 23:20:08 +0900

    Update board.service.js

--- service/board.service.js
+++ service/board.service.js
@@ -1,16 +1,19 @@
 // 2. 서버안에 기능을 만드는 부분
 
-const pool = require("../db");
+const pool = require("../db"); // sql 실행하기
 
+// 1.  findAll: 전체조회
+// row2: 게시글 목록
 const service = {
   findAll: async function () {
     let [rows, result] = await pool.query("select * from board"); // 배열 구조분해 (비동기처리 방식)
     console.log(rows);
-    return rows;
+    return rows; // controller로 rows 전달
   },
 
-  // id 단건 조회 기능 만들기
+  // id 단건 조회 기능 만들기 (id에 해당하는 게시글 조회)
   findById: async function (id) {
+    // board 테이블에서 설정한 id 값인 게시글을 조회
     let [req1, res2] = await pool.query("select * from board where id = ?", id);
     console.log(req1);
     return req1;
@@ -22,11 +25,11 @@ const service = {
       [title, content, writer], // valuse(?,?,?) 에 전달할 값 (배열 형태로 전달)
     );
     console.log(result);
-    return result[0].insertId;
+    return result[0].insertId; // DB에 추가된 행의 id값을 돌려줌
   },
   // 내용 업데이트
   update: async function (data = {}) {
-    const { title, content, id } = data; // 객체의 구조분해
+    const { title, content, id } = data; // 앞에서 가져온 값을 참조하기 위해 한번더 적음
     console.log(title, content, id);
     let result = await pool.query(
       "Update board set title = ?, content = ? where id = ?",
@@ -37,12 +40,9 @@ const service = {
   },
   // 내용 삭제
   remove: async function (id) {
-    let [req3, res4] = await pool.query(
-      "select * from board where id = ? ",
-      id,
-    );
+    let [result] = await pool.query("delete * from board where id = ? ", [id]);
     console.log(req3);
-    return req3.delete;
+    return result.affectedRows;
   },
 };
 module.exports = service;
